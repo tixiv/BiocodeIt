20bac60cb536616064109a689abef1e613b53d39
Krelinos <30482246+Krelinos@users.noreply.github.com>
2022-04-03 21:14:45 -0500

    Initial commit

---
+++ Source/BiocodeIt/BiocodeIt_Settings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Krelinos_BiocodeIt
+{
+
+    // Just so you know, I basically Copy & Pasted these two classes from the Mod Settings section of the Rimworld Wiki Modding Tutorials
+    public class BiocodeIt_Settings : ModSettings
+    {
+        public static int biocodedStealChancePercent = 0;
+        public static int biocodedMarketValuePercent = 0;
+        public static bool notifyPlayerOfSpite = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref biocodedMarketValuePercent, "biocodedMarketValuePercent", 35);
+            Scribe_Values.Look(ref biocodedStealChancePercent, "biocodedStealChancePercent", 0);
+            Scribe_Values.Look(ref notifyPlayerOfSpite, "notifyPlayerOfSpite", true);
+            base.ExposeData();
+        }
+    }
+
+    public class BiocodeIt : Mod
+    {
+        BiocodeIt_Settings settings;
+        public BiocodeIt(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<BiocodeIt_Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+
+            listingStandard.Label(String.Format("BiocodedMarketValuePercentLabel".Translate(), BiocodeIt_Settings.biocodedMarketValuePercent));
+            BiocodeIt_Settings.biocodedMarketValuePercent = (int)listingStandard.Slider(BiocodeIt_Settings.biocodedMarketValuePercent, 0f, 100f);
+            
+            listingStandard.Label(String.Format("BiocodedStealChancePercentLabel".Translate(), BiocodeIt_Settings.biocodedStealChancePercent));
+            BiocodeIt_Settings.biocodedStealChancePercent = (int)listingStandard.Slider(BiocodeIt_Settings.biocodedStealChancePercent, 0f, 100f);
+
+            listingStandard.Label(String.Format("BiocodedStealChancePercentExplanation".Translate(),
+                Math.Round(BiocodeIt_Settings.biocodedStealChancePercent / 100f, 2),
+                Math.Round(
+                    (1 - Math.Pow((100 - BiocodeIt_Settings.biocodedStealChancePercent) / 100f, 5)) * 100,
+                1)
+            ));
+
+            listingStandard.CheckboxLabeled("BiocodedNotifyPlayerOfSpiteLabel".Translate(), ref BiocodeIt_Settings.notifyPlayerOfSpite);
+
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "BiocodeIt_SettingsCategory".Translate();
+        }
+    }
+
+}
---
+++ Source/BiocodeIt/HarmonyPatch.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace Krelinos_BiocodeIt
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatch
+    {
+        static HarmonyPatch()
+        {
+            var harmony = new Harmony("krelinos.BiocodeIt");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(StealAIUtility))]
+    [HarmonyLib.HarmonyPatch("TryFindBestItemToSteal")]
+    class Patch_IgnoreBiocodedThingsWhenStealing
+    {
+        static void Postfix(IntVec3 root, Map map, float maxDist, ref Thing item, Pawn thief, List<Thing> disallowed, ref bool __result)
+        {
+            if (__result)
+            {
+                if (CompBiocodable.IsBiocoded(item))
+                {
+                    if (UnityEngine.Random.Range(0, 100) > BiocodeIt_Settings.biocodedStealChancePercent)
+                    {
+                        if( disallowed == null)
+                        {
+                            disallowed = new List<Thing>();
+                        }
+
+                        disallowed.Add(item);   // Blacklist the item from future scans...
+                        // ...And find a different Thing to steal
+                        __result = StealAIUtility.TryFindBestItemToSteal(root, map, maxDist, out item, thief, disallowed);
+                    }
+                    else
+                    {
+                        if( BiocodeIt_Settings.notifyPlayerOfSpite)
+                        {
+                            Messages.Message(String.Format("BiocodedNotifyPlayerOfSpiteAlert".Translate(), thief.NameShortColored, item.LabelShort), thief, MessageTypeDefOf.NegativeEvent, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(StatPart_Biocoded))]
+    [HarmonyLib.HarmonyPatch("TransformValue")]
+    class Patch_BiocodedThingsStillHaveValueMaybe_ItDependsOnYourSettings
+    {
+        static bool Prefix(StatRequest req, ref float val)
+        {
+            if (req.HasThing && CompBiocodable.IsBiocoded(req.Thing))
+            {
+                val *= BiocodeIt_Settings.biocodedMarketValuePercent / 100f;
+            }
+            return false;
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(StatPart_Biocoded))]
+    [HarmonyLib.HarmonyPatch("ExplanationPart")]            // If you ever get an error about an "Undefined target method for patch method",
+               // it's because this ^ is mispelt... hhhhhhhhhhhhhhhhhhhhhhh
+    class Patch_ExplainWhyBiocodedThingsAreMaybeWorthLessThanNonBiocodedThings
+    {
+        static void Postfix(StatRequest req, ref string __result)
+        {
+            if (req.HasThing && CompBiocodable.IsBiocoded(req.Thing))
+            {
+                __result = "BiocodedMarketValueMultiplier".Translate() + ": x" + BiocodeIt_Settings.biocodedMarketValuePercent + "%";
+            }
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(StatWorker))]
+    [HarmonyLib.HarmonyPatch("DisplayTradeStats")]
+    class Patch_BiocodedWeaponsNowShowMarketValue
+    {
+        static void Postfix(StatRequest req, ref bool __result)
+        {
+            if (!__result)
+            {
+                if( CompBiocodable.IsBiocoded(req.Thing))
+                {
+                    __result = true;
+                }
+            }
+//            ThingDef thingDef;
+//            __result = (thingDef = (req.Def as ThingDef)) != null
+//                && !req.HasThing                                                                                    // Yeah... this is a copy and paste from
+//                && (                                                                                                // the unpatched function but the check
+//                    (thingDef.category == ThingCategory.Building && thingDef.Minifiable)                            // for biocode is altered. If you ever wondered
+//                    || (TradeUtility.EverPlayerSellable(thingDef) || CompBiocodable.IsBiocoded(req.Thing))          // why the game never reports the value of
+//                    || (thingDef.tradeability.TraderCanSell()                                                       // non-minifiable builings, this is why.
+//                        && (thingDef.category == ThingCategory.Item || thingDef.category == ThingCategory.Pawn)     // Maybe I'll make a mod that lets you.
+//                       )                                                                                            // Or hey, maybe you can!
+//                   );
+        }
+    }
+}
