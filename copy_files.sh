5832cc5bbaee3a233a309048f6ea7d2edab7406f
Peter Fuhrmann <tixiv@gmx.net>
2025-06-13 15:32:40 +0200

    Update PublishedFileId, add copy script

---
+++ copy_files.sh
@@ -0,0 +1,6 @@
+cp -r About /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+cp -r Assemblies /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+cp -r Defs /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+cp -r Languages /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+cp LICENSE /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+
