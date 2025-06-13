3124867f2213f7efa8bbfaf68d9eb7a6c7689094
Peter Fuhrmann <tixiv@gmx.net>
2025-06-13 16:09:40 +0200

    Add support for game version 1.6

---
+++ Source/BiocodeIt_1_6/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("BiocodeIt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BiocodeIt")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("8ce265c6-3b1e-4b5b-9273-2a11410f8a05")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- copy_files.sh
+++ copy_files.sh
@@ -1,5 +1,6 @@
 cp -r About /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
-cp -r Assemblies /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+cp -r "1.5" /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
+cp -r "1.6" /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
 cp -r Defs /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
 cp -r Languages /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
 cp LICENSE /mnt/e/SteamLibrary/steamapps/common/RimWorld/Mods/BiocodeIt/
